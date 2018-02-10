b9dfe32d538f50a025ed17214ca31638125af31d
Andy Gonzalez <agonza7@students.towson.edu>
2018-02-10 16:23:00 -0500

    Task2 Project1

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author agonc
+ * This is task 2, of Project 1
+ * Andy Gonzalez-Campos
+ */
+public class HelloWorld {
+	/**
+	 * This is the Main Class which prints "Hello World"
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+	}
+
+}
